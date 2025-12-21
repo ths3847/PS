54cc860ecfccb1cf0b824c10c7e2e68095dbe96f
seosg <46395571+ths3847@users.noreply.github.com>
2025-12-21 14:45:22 +0000

    solve: BOJ 13913

---
+++ src/dfs_bfs/solved_BOJ_13913.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> v(100001);
+vector<int> p(100001, -1);
+int N, M;
+int main(){
+    vector<int> res;
+    cin >> N >> M;
+    queue<int> q;
+    q.push(N);
+    p[N] = 0;
+    v[N] = 1;
+    while(q.size()){
+        int x = q.front();
+        q.pop();
+        for(int i=0;i<3;i++){
+            if(i==0){
+                if(x-1 >= 0 && v[x-1] == 0){
+                    v[x-1] = v[x]+1;
+                    q.push(x-1);
+                    p[x-1] = x;
+                }
+            }
+            else if(i==1){
+                if(x+1 <= M && v[x+1] == 0){
+                    v[x+1] = v[x]+1;
+                    q.push(x+1);
+                    p[x+1] = x;
+                }
+            }
+            else{
+                if(x*2 <= 100001 && v[x*2] == 0){
+                    v[x*2] = v[x]+1;
+                    q.push(x*2);
+                    p[2*x] = x;
+                }
+            }
+        }
+    }
+
+    cout << v[M]-1 << endl;
+    int tmp = M;
+    //부모 확인
+    while(tmp != N){
+        res.push_back(tmp);
+        tmp = p[tmp];
+    }
+    cout << N << " ";
+    for(int i = res.size()-1; i>=0;i--){
+        cout << res[i];
+        if( i!=0) cout << " ";
+    }
+    return 0;
+}
